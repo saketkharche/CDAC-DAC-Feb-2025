1898ffba1bd070a11b873319263a69cc8f949fa1
Saket M Kharche <kharchesaket@gmail.com>
2025-02-23 12:05:12 +0530

    commit

--- Logic Building Session/Day 03/Q2FactorialOfTen.java
+++ Logic Building Session/Day 03/Q2FactorialOfTen.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-    class Q2FactorialOfTen {
-=======
 class Q2FactorialOfTen {
->>>>>>> b0f2c3c65d5222b5384f06a54ac9090bd351fb3a
     public static void main(String[] args) {
         long factorial = 1; // Use long to avoid integer overflow
         for (int i = 1; i <= 10; i++) {
@@ -11,4 +7,5 @@ class Q2FactorialOfTen {
         System.out.println("Factorial of 10: " + factorial);
     }
 }
+
 // Sample Output: Factorial of 10: 3628800
--- Logic Building Session/Day 03/Q6FirstFivePrimes.java
+++ Logic Building Session/Day 03/Q6FirstFivePrimes.java
@@ -6,11 +6,7 @@ class Q6FirstFivePrimes {
 
         while (count < 5) {
             boolean isPrime = true;
-<<<<<<< HEAD
             for (int i = 2; i * i <= num; i++) {
-=======
-            for (int i = 2; i <= Math.sqrt(num); i++) {
->>>>>>> b0f2c3c65d5222b5384f06a54ac9090bd351fb3a
                 if (num % i == 0) {
                     isPrime = false;
                     break;
@@ -26,9 +22,7 @@ class Q6FirstFivePrimes {
         System.out.println();
     }
 }
-<<<<<<< HEAD
-=======
+
 // Sample Output:
 // First 5 prime numbers:
 // 2 3 5 7 11
->>>>>>> b0f2c3c65d5222b5384f06a54ac9090bd351fb3a
